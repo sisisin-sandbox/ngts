d77e25d6a13248d7af33f3990c439e5f1329dc40
sisisin <azsisisin@gmail.com>
2016-08-08 19:12:31 +0900

    karma works.

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'browserify'],
+    files: [
+      'dist/bundle.js',
+      'test/karma/**-spec.ts'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'test/karma/**-spec.ts': ['browserify']
+    },
+    browserify:{
+      debug: true,
+      plugin: ['tsify'],
+      transform: ['espowerify'],
+      extensions: ['.ts', '.js']
+    },
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
---
+++ test/karma/controller-spec.ts
@@ -0,0 +1,9 @@
+import assert = require('power-assert');
+import {HelloController} from '../../src/controller';
+
+describe('karma', () => {
+  it('works!', () => {
+    let hoge = <HelloController>{};
+    assert(true);    
+  });
+});
