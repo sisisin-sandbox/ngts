d8225309b8d7771eb08fd7d9a7080dad2dd00b9f
sisisin <azsisisin@gmail.com>
2016-12-20 17:25:11 +0900

    sample: sample badge

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # angular ts test
 
 ![cov-badge](https://s3-ap-northeast-1.amazonaws.com/zoi-public/branch/master.svg)
+![cov-badge](https://s3-ap-northeast-1.amazonaws.com/zoi-public/branch/package-a.svg)
--- scripts/put-coverage-summary-json.js
+++ scripts/put-coverage-summary-json.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 const exec = require('child_process').exec;
 const {buildSummary, getSummaryJsonPath} = require('./coverage-util');
 
-const cov = { coverage: buildSummary().statements.pct };
+const cov = buildZoiCoverage(process.argv[2]);
+
+function buildZoiCoverage(subject = 'master') {
+  const summary = buildSummary();
+  const coverage = subject === 'master' ? summary.statements.pct : 15;
+  const color = subject === 'master' ? 'green' : 'red';
+  return { coverage, subject, color };
+}
+
 const summaryPath = getSummaryJsonPath();
 fs.writeFile(summaryPath, JSON.stringify(cov), (err) => {
   if (err) {
@@ -10,7 +18,7 @@ fs.writeFile(summaryPath, JSON.stringify(cov), (err) => {
     process.exit(1);
   }
   exec(`aws s3 cp ${summaryPath} s3://zoi-coverage/`, (err, stdout, stderr) => {
-    if(err) {
+    if (err) {
       console.log(err);
       process.exit(1);
     }
