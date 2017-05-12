154c148a7413b6d63582e04a8b414b9b7afb18f5
sisisin <azsisisin@gmail.com>
2017-05-12 20:00:20 +0900

    refactor: to bootstrap

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ angular
       .when('/login', {
         template: '<app-text-component></app-text-component>'
       })
-      .when('/', {template: 'hogehogehoge'});
+      .when('/', {template: 'hogehogehoge'}));
 
 import './modules/controller';
 import './modules/component';
 import './modules/directive';
 import './modules/service';
+
+angular.bootstrap(document.body, ['app']);
