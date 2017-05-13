5ee3adc51a7dfa5321e8d0ecffb28bb0e847cccd
sisisin <azsisisin@gmail.com>
2017-05-13 13:25:10 +0900

    feat: create mix ng1 and ng2 component,service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,57 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component, Pipe, PipeTransform, Injectable, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { downgradeComponent, downgradeInjectable } from '@angular/upgrade/static';
+
+@Injectable()
+export class BarService {
+  bar() {
+    console.log('bar');
+  }
+}
+angular.module('app').service('BarService', downgradeInjectable(BarService));
+
+@Component({
+  selector: 'app-ng2-component',
+  template: `<div>my ng2 component<app-child></app-child></div>`
+})
+class AppNg2Component implements OnInit {
+  constructor(private barService: BarService) { }
+  ngOnInit() {
+    this.barService.bar();
+  }
+}
+angular.module('app').directive('appNg2Component', downgradeComponent({ component: AppNg2Component }));
+
+@Component({
+  selector: 'app-child',
+  template: `<div>{{text | foopipe}}</div>`
+})
+class ChildComponent {
+  text = 'child!!!';
+}
+
+
+@Pipe({ name: 'foopipe' })
+class FooPipe implements PipeTransform {
+  transform(input: string) {
+    return input.toUpperCase();
+  }
+}
+angular.module('app').filter('foopipe', downgradeInjectable(FooPipe));
 
 @NgModule({
   imports: [
     BrowserModule,
     UpgradeModule
-  ]
+  ],
+  declarations: [AppNg2Component, ChildComponent, FooPipe],
+  providers: [BarService],
+  entryComponents: [AppNg2Component]
 })
 export class AppModule {
-  ngDoBootstrap() {}
+  ngDoBootstrap() { }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
